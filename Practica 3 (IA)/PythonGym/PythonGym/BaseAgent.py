3f78962e50c6661db9713b93bf32d399cef019e9
DEIVISS <99610648+Lizzen@users.noreply.github.com>
2025-02-27 17:39:41 +0100

    test 1 va (muy mejorable, calidad pokemon za)

--- Practica 3 (IA)/PythonGym/PythonGym/BaseAgent.py
+++ Practica 3 (IA)/PythonGym/PythonGym/BaseAgent.py
@@ -1,4 +1,5 @@
 import random
+import time
 class BaseAgent:
     def __init__(self, id, name):
         self.id = id
@@ -22,17 +23,24 @@ class BaseAgent:
         action = 0 
         fire = False
 
-        if perception[8] > perception[12]:
-            action = 3
-        elif perception[13] > perception[9]:
-            action = 2
-        elif perception[8] < perception[12]:
-            action = 4
-        elif perception[9] < perception[13]:
-            action = 1
+        for i in range(0,4):
+            if action == 0:
+                if i == 0 and perception[8] > perception[12]:  # Der
+                    if perception[2] == 0 or perception[6] > 1.00:
+                        action = 3
+                elif i == 2 and perception[13] > perception[9]: # Abajo
+                    if perception[1] == 0 or perception[5] > 1.00:
+                        action = 2
+                elif i == 3 and perception[8] < perception[12]: # Izq
+                    if perception[3] == 0 or perception[7] > 1.00:
+                        action = 4
+                elif i == 1 and perception[13] < perception[9]: # Arriba
+                    if perception[0] == 0 or perception[4] > 1.00:
+                        action = 1
 
-        if perception[14] and (perception[8] == perception[12] or perception[9] == perception[13]):
+        if perception[14] and (int(perception[8]) == int(perception[12]) or int(perception[9]) == int(perception[13])): #Puede dispara y le tiene a tiro
             fire = True
+
 
         return action, fire
     
